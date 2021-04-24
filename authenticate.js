2a79d8775ea9939334bcdf9290bbbd7b9c532901
Prince Segbefia <princeasegbefia@gmail.com>
2021-04-24 09:34:14 -0600

    Setup verifyAdmin() middleware

--- authenticate.js
+++ authenticate.js
@@ -5,10 +5,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
 
-
 const config =require('./config');
-
-
 
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
@@ -43,3 +40,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', { session: false});
+exports.verifyAdmin = function(req, res, next){
+    if (req.user.admin) {
+        return next();
+    }
+    else {
+       const err = new Error('You are not authorized to perform this operation');
+       err.status = 403;
+       return next(err)
+    }
+}
