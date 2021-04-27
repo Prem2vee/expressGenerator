312feeff7dd9dcbd68028476dc8aa1807a66a844
Prince Segbefia <princeasegbefia@gmail.com>
2021-04-27 11:09:39 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3010344799202354',
+        clientSecret: 'a181d710648b2257f1075cc9756da63b'
+    }
 }
